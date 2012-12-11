1641c1b934146e221cf084d22f9cb245f82603b1
Ross Rowe <piaross@gmail.com>
2012-12-11 22:31:06 +1100

    Added mandatory field unit test

--- test/unit/com/nerderg/goodForm/FormDataServiceTests.groovy
+++ test/unit/com/nerderg/goodForm/FormDataServiceTests.groovy
@@ -1,22 +1,41 @@
 package com.nerderg.goodForm
 
+import com.nerderg.goodForm.form.Form
+import com.nerderg.goodForm.form.Question
 import grails.test.mixin.TestFor
+import grails.test.mixin.TestMixin
+import grails.test.mixin.web.ControllerUnitTestMixin
+import org.junit.Before
 
 /**
  * @author Ross Rowe
  */
 @TestFor(FormDataService)
+@TestMixin(ControllerUnitTestMixin)
 class FormDataServiceTests {
 
+    @Before public void setUp() {
+        service.goodFormService = new GoodFormService()
+    }
+
     void testMandatoryFieldValidation() {
-        fail "implement me"
+        Form f = new Form()
+        f.question("Q1") {
+            "Given Name" text: 50, required: true
+        }
+        Question question = f.getAt("Q1")
+        def formInstance = new FormInstance()
+        def error = service.validateAndProcessFields(question.formElement, [:], formInstance)
+        assertTrue("Error was not detected", error)
+        error = service.validateAndProcessFields(question.formElement, ['Q1': ['Given_Name':'Joe']], formInstance)
+        assertFalse("Error was detected", error)
     }
 
     void testInvalidDateField() {
-       fail "implement me"
+        fail "implement me"
     }
 
     void testInvalidPatternField() {
-       fail "implement me"
+        fail "implement me"
     }
 }
