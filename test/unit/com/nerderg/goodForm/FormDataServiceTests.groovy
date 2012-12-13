0a7aafbd0bf9a3089afebd365742b36ea55fe799
Ross Rowe <piaross@gmail.com>
2012-12-13 21:16:29 +1100

    Added additional tests

--- test/unit/com/nerderg/goodForm/FormDataServiceTests.groovy
+++ test/unit/com/nerderg/goodForm/FormDataServiceTests.groovy
@@ -27,15 +27,34 @@ class FormDataServiceTests {
         def formInstance = new FormInstance()
         def error = service.validateAndProcessFields(question.formElement, [:], formInstance)
         assertTrue("Error was not detected", error)
-        error = service.validateAndProcessFields(question.formElement, ['Q1': ['Given_Name':'Joe']], formInstance)
+        error = service.validateAndProcessFields(question.formElement, ['Q1': ['Given_Name': 'Joe']], formInstance)
         assertFalse("Error was detected", error)
     }
 
     void testInvalidDateField() {
-        fail "implement me"
+        Form f = new Form()
+        f.question("Q1") {
+            "Date Of Birth" date: "d/M/yyyy"
+        }
+        Question question = f.getAt("Q1")
+        def formInstance = new FormInstance()
+        def error = service.validateAndProcessFields(question.formElement, ['Q1': ['Date_Of_Birth': '2012/01/01']], formInstance)
+        //TODO this actually parses into a date, but not the expected value - should the validator handle this somehow?
+        assertTrue("Error was not detected", error)
+        error = service.validateAndProcessFields(question.formElement, ['Q1': ['Given_Name': '01/01/2012']], formInstance)
+        assertFalse("Error was detected", error)
     }
 
     void testInvalidPatternField() {
-        fail "implement me"
+        Form f = new Form()
+        f.question("Q1") {
+            "Given Name" text: 50, pattern: /[A-Za-z]+/
+        }
+        Question question = f.getAt("Q1")
+        def formInstance = new FormInstance()
+        def error = service.validateAndProcessFields(question.formElement, ['Q1': ['Given_Name': '1234']], formInstance)
+        assertTrue("Error was not detected", error)
+        error = service.validateAndProcessFields(question.formElement, ['Q1': ['Given_Name': 'Joe']], formInstance)
+        assertFalse("Error was not detected", error)
     }
 }
