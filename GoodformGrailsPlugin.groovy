b42ac23fab937d9c6812d2baf05e30897d07c9de
pmcneil <peter@mcneils.net>
2013-03-06 20:55:58 +1100

    Start cleaning up the cruft for the plugin release.

--- GoodformGrailsPlugin.groovy
+++ GoodformGrailsPlugin.groovy
@@ -2,21 +2,15 @@ class GoodformGrailsPlugin {
     // the plugin version
     def version = "1.0.0-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.1 > *"
+    def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
 
     def loadAfter = ["rendering"]
     // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp",
-            "web-app/css/ui-lightness",
-            "web-app/css/errors.css",
-            "web-app/css/main.css",
-            "web-app/css/mobile.css"
-    ]
+    def pluginExcludes = []
 
     def title = "Goodform Plugin" // Headline display name of the plugin
-    def author = "Peter McNeil, Ross Rowe"
+    def author = "Peter McNeil"
     def authorEmail = "peter@nerderg.com"
     def description = '''\
 Create extremely usable complex forms with rules based flow that work well in browsers.
@@ -29,48 +23,18 @@ Good form features a form definition DSL that allows rapid human readable form c
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "nerdEg Pty Ltd", url: "http://www.nerderg.com/" ]
 
     // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def developers = [ [ name: "Ross Rowe", email: "ross@nerderg.com" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "JIRA", url: "https://github.com/nerdErg/GoodForm/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/nerdErg/GoodForm" ]
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
