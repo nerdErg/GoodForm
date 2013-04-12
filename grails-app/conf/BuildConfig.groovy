6554e3c269c2d2724a9623cfffedebd1b914b5e6
pmcneil <peter@mcneils.net>
2013-04-12 10:07:19 +1000

    remove commented out plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,7 +32,6 @@ grails.project.dependency.resolution = {
 
         // including rendering plugin as a compile dependency seems to cause loading problems
         compile ":rendering:0.4.3",
-//                  ":cxf:1.0.7",
                   ":jquery:1.8.3",
                   ":jquery-ui:1.8.24",
                   ":nerderg-form-tags:2.1.3"
