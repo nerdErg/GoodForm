9854216e5f3a0e441882c0aa46afaa7db70919d4
Ross Rowe <piaross@gmail.com>
2013-04-22 22:10:47 +1000

    Added some more unit tests

--- test/unit/com/nerderg/goodForm/FormTagLibTests.groovy
+++ test/unit/com/nerderg/goodForm/FormTagLibTests.groovy
@@ -1,12 +1,36 @@
 package com.nerderg.goodForm
-
+import com.nerderg.goodForm.form.FormElement
+import com.nerderg.taglib.NerdergFormtagsTagLib
 import grails.test.mixin.TestFor
+import grails.test.mixin.TestMixin
+import grails.test.mixin.services.ServiceUnitTestMixin
+import org.junit.Before
 
 @TestFor(FormTagLib)
+@TestMixin(ServiceUnitTestMixin)
 class FormTagLibTests {
 
+    FormElement formElement
 
-    void testFilterName() {
+    def goodFormService
+
+    @Before
+    void setUp() {
+
+        tagLib.goodFormService = new GoodFormService()
+        mockTagLib(NerdergFormtagsTagLib.class)
+    }
+
+    void testPick() {
+        formElement = new FormElement()
+        formElement.form("This is a test", [pick: 1, map: 'test'], null) {
+            "are you male?" default: true
+            "female"()
+            "other"() {
+                "well what the heck are you?" text: 80, map: 'other'
+            }
+        }
+        String result = applyTemplate('<form:element element="${formElement}" store="${formData}"/>', [formElement: formElement, formData: [:]]).trim()
 
     }
 }
