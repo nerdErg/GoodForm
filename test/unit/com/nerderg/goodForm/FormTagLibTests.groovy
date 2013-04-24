c931750da00acb5dfdfde9b79d645c779e9fbe23
Ross Rowe <piaross@gmail.com>
2013-04-24 21:48:11 +1000

    Added some more unit tests

--- test/unit/com/nerderg/goodForm/FormTagLibTests.groovy
+++ test/unit/com/nerderg/goodForm/FormTagLibTests.groovy
@@ -15,13 +15,12 @@ class FormTagLibTests {
 
     @Before
     void setUp() {
-
+        formElement = new FormElement()
         tagLib.goodFormService = new GoodFormService()
         mockTagLib(NerdergFormtagsTagLib.class)
     }
 
     void testPick() {
-        formElement = new FormElement()
         formElement.form("Dummy element", [pick: 1, map: 'test'], null) {
             "are you male?" default: true
         }
@@ -32,7 +31,6 @@ class FormTagLibTests {
     }
 
     void testHeader() {
-        formElement = new FormElement()
         formElement.form("This is a test", [heading: 1, map: 'test'], null) {
             "A Test Heading"
         }
@@ -41,11 +39,42 @@ class FormTagLibTests {
     }
 
     void testNumber() {
-        formElement = new FormElement()
         formElement.form("This is a test", [number: 5, map: 'test'], null) {
-            "A Test Heading"
+            "12345"
         }
         String result = applyTemplate('<form:element element="${formElement}" store="${formData}"/>', [formElement: formElement, formData: [:]]).trim()
         assert result.contains('input type=\'number\'')
+    }
+
+    void testPhone() {
+        formElement.form("This is a test", [phone: 15, map: 'test'], null) {
+            "+61400123456"
+        }
+        String result = applyTemplate('<form:element element="${formElement}" store="${formData}"/>', [formElement: formElement, formData: [:]]).trim()
+        assert result.contains('input type=\'tel\'')
+    }
+
+    void testMoney() {
+        formElement.form("This is a test", [money: 5, map: 'test'], null) {
+            "123.00"
+        }
+        String result = applyTemplate('<form:element element="${formElement}" store="${formData}"/>', [formElement: formElement, formData: [:]]).trim()
+        assert result.contains('class=\'money\'')
+    }
+
+    void testDate() {
+        formElement.form("This is a test", [date: "d/M/yyyy", map: 'test'], null) {
+            "01/01/2013"
+        }
+        String result = applyTemplate('<form:element element="${formElement}" store="${formData}"/>', [formElement: formElement, formData: [:]]).trim()
+        assert result.contains('class=\'date\'')
+    }
+
+    void testDatetime() {
+        formElement.form("This is a test", [datetime: "d/M/yyyy", map: 'test'], null) {
+            "01/01/2013"
+        }
+        String result = applyTemplate('<form:element element="${formElement}" store="${formData}"/>', [formElement: formElement, formData: [:]]).trim()
+        assert result.contains('class=\'date\'')
     }
 }
