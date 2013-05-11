82569c7d3cb66f823e9ada42913af35d599c554a
pmcneil <peter@mcneils.net>
2013-05-11 23:59:31 +1000

    Fix #13 Apply the form behaviour specifically to form elements scoped by div.goodFormContainer. Apply behaviour to the copied dom fragment. Added a work around for the nerdergFormTags datepicker. The id's set in nerdergFormTags are copied with the fragment which is generally bad. For now we just remove the ids on the date inputs to make this work. In general the formTags need a clean up that might make them a better fit. Bump the plugin version to 1.0.1

--- GoodformGrailsPlugin.groovy
+++ GoodformGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class GoodformGrailsPlugin {
-    def version = "1.0.0"
+    def version = "1.0.1"
     def grailsVersion = "2.0 > *"
 
     def loadAfter = ["rendering"]
--- web-app/js/goodform.js
+++ web-app/js/goodform.js
@@ -1,25 +1,141 @@
 (goodform = {
     baseContextPath: '/',
-    active:0,
-    x:0,
-    y:0,
-    postData:function (uri, data, func, opts) {
+    active: 0,
+    x: 0,
+    y: 0,
+    postData: function (uri, data, func, opts) {
         $.ajax({
-            type:"POST",
-            url:uri,
-            data:data,
-            success:function (resp) {
+            type: "POST",
+            url: uri,
+            data: data,
+            success: function (resp) {
                 func(resp, opts);
             }
         });
     },
-    postForm:function (form, func) {
+    postForm: function (form, func) {
         return this.postData(form.action, $(form).serialize(), func)
+    },
+    addBehaviour: function (parent) {
+        parent.find('span.hiddenDetails').each(function () {
+            var offset = $(this).parent().position();
+            var x = offset.left + 20;
+            var y = offset.top + 20;
+            $(this).css('left', x + 'px').css('top', y + 'px');
+        });
+
+        parent.find('span.quantity').hover(function (event) {
+            $(this).children('span.hiddenDetails').show();
+        }, function (event) {
+            $(this).find('span').hide();
+        });
+
+        parent.find('.revealNext').click(function () {
+            $(this).next('div.hiddenDiv').slideToggle("medium");
+        });
+
+        parent.find('.revealNexthi').click(function () {
+            $(this).next('div.hiddenDiv').toggle('highlight', 500);
+        });
+
+        parent.find('.hiddenFormCheckbox').change(function (event) {
+            var parentDiv = $(this).parents('div.inlineCheck')
+            if ($(this).attr('checked')) {
+                $(parentDiv).next('div').show();
+            } else {
+                $(parentDiv).next('div').hide();
+            }
+        });
+
+        parent.find('.hiddenFormRadio').change(function (event) {
+            var parentDiv = $(this).parents('div.prop')
+            if ($(this).attr('checked')) {
+                $(parentDiv).next('div').show();
+            } else {
+                $(parentDiv).next('div').hide();
+            }
+        });
+
+        parent.find(".suggest").each(function () {
+            var action = $(this).attr("class").split(" ")[1];
+            var actionurl = goodform.baseContextPath + '/suggest/' + action;
+            $(this).autocomplete({
+                minLength: 1,
+                source: actionurl,
+                select: function (event, ui) {
+                    $(this).val(ui.item.value);
+                }
+            });
+        });
+
+        parent.find("input[type='file']").change(function () {
+            var name = $(this).val().replace(/^.*(\\|\/)/, '');
+            $(this).next('span').html(name);
+        });
+
+        parent.find('.addAnotherForm').click(function (event) {
+            var container = $(this).prev('div.listContainer');
+            var formDiv = $(container).children('div.questionListOfItem').first();
+            //need to remove the datepicker before we copy then add it back
+            formDiv.find('input.date').datepicker("destroy");
+            var cloneForm = $(formDiv.clone());
+            cloneForm.find("input[type='text'],input[type='number']").val('');
+            cloneForm.appendTo(container);
+            goodform.addBehaviour(cloneForm);
+            goodform.addNerdergFormTagsBehaviour(cloneForm);
+            goodform.addNerdergFormTagsBehaviour(formDiv);
+            cloneForm.children('.removeForm').click(function (event) {
+                var container = $(this).parent('div.questionListOfItem').parent('div.listContainer');
+                if (container.children('div.questionListOfItem').length > 1) {
+                    $(this).parent('div.questionListOfItem').remove();
+                } else {
+                    $(this).parent('div.questionListOfItem').find("input[type='text'],input[type='number']").val('');
+                }
+            });
+        });
+
+        parent.find('.removeForm').click(function (event) {
+            var container = $(this).parent('div.questionListOfItem').parent('div.listContainer');
+            if (container.children('div.questionListOfItem').length > 1) {
+                $(this).parent('div.questionListOfItem').remove();
+            } else {
+                $(this).parent('div.questionListOfItem').find("input[type='text'],input[type='number']").val('');
+            }
+        });
+
+        parent.find('div.qset').click(function (event) {
+            var url = $(this).data('backurl');
+            window.location = url;
+        });
+
+        parent.find('div.inlineCheck').filter(':even').css('background', '#E5E4E8');
+
+    },
+    addNerdergFormTagsBehaviour: function(parent) {
+        var dateInp = parent.find('input.date');
+        dateInp.removeAttr('id'); //datepicker adds it's own id
+        dateInp.datepicker({
+            dateFormat: nerdergFormTags.dateFormat,
+            showOn: 'button',
+            buttonImage: nerdergFormTags.dateImg,
+            buttonImageOnly: true,
+            changeMonth: true,
+            changeYear: true
+        });
+    },
+    fixNerdergFormTagsDatepickers: function() {
+        $('input.date').datepicker("destroy").removeAttr('id').datepicker({
+            dateFormat: nerdergFormTags.dateFormat,
+            showOn: 'button',
+            buttonImage: nerdergFormTags.dateImg,
+            buttonImageOnly: true,
+            changeMonth: true,
+            changeYear: true
+        });
     }
 
 });
 
-var workTypes = [];
 $(function () {
     $('body').mousemove(function (e) {
         goodform.x = e.pageX;
@@ -45,137 +161,9 @@ $(function () {
         }
     });
 
-    $('span.hiddenDetails').each(function () {
-        var offset = $(this).parent().position();
-        var x = offset.left + 20;
-        var y = offset.top + 20;
-        $(this).css('left', x + 'px').css('top', y + 'px');
-    });
-
-    $('span.quantity').hover(function (event) {
-        $(this).children('span.hiddenDetails').show();
-    }, function (event) {
-        $(this).find('span').hide();
-    });
-
-    $('.revealNext').click(function () {
-        $(this).next('div.hiddenDiv').slideToggle("medium");
-    });
-    $('.revealNexthi').click(function () {
-        $(this).next('div.hiddenDiv').toggle('highlight', 500);
-    });
-
-    $('.paycb').click(function () {
-        $(this).hide();
-        $(this).next('div.hiddenForm').show();
-        $(this).next('div.hiddenForm').find('input').removeAttr('disabled');
-        $('#claim').removeAttr('disabled');
-        goodform.active++;
-    });
-    $('.paycbx').click(function () {
-        $(this).parents('div.hiddenForm').prev('img.paycb').show();
-        $(this).parents('div.hiddenForm').hide();
-        $(this).prev('input').attr('disabled', 'disabled');
-        if (--goodform.active == 0) {
-            $('#claim').attr('disabled', 'disabled');
-        }
-    });
-
-    $('.hiddenFormCheckbox').change(function (event) {
-        var parentDiv = $(this).parents('div.inlineCheck')
-        if ($(this).attr('checked')) {
-            $(parentDiv).next('div').show();
-        } else {
-            $(parentDiv).next('div').hide();
-        }
-    });
-
-    $('.invertedHiddenFormCheckbox').change(
-        function (event) {
-            var parentDiv = $(this).parents('div.inlineCheck')
-            if ($(this).attr('checked')) {
-                $(parentDiv).next('div').hide();
-            } else {
-                $(parentDiv).next('div').show();
-            }
-        }).parents('div.prop').next('div').hide();
-
-    $('.hiddenFormRadio').change(function (event) {
-        var parentDiv = $(this).parents('div.prop')
-        if ($(this).attr('checked')) {
-            $(parentDiv).next('div').show();
-        } else {
-            $(parentDiv).next('div').hide();
-        }
-    });
-
-    $('.invertedHiddenRadio').change(
-        function (event) {
-            var parentDiv = $(this).parents('div.prop')
-            if ($(this).attr('checked')) {
-                $(parentDiv).next('div').hide();
-            } else {
-                $(parentDiv).next('div').show();
-            }
-        }).parents('div.prop').next('div').hide();
-
-    $(".suggest").each(function () {
-        var action = $(this).attr("class").split(" ")[1];
-        var actionurl = goodform.baseContextPath + '/suggest/' + action;
-        $(this).autocomplete({
-            minLength:1,
-            source:actionurl,
-            select:function (event, ui) {
-                $(this).val(ui.item.value);
-            }
-        });
-    });
-
-    $("input[type='file']").change(function () {
-        var name = $(this).val().replace(/^.*(\\|\/)/, '');
-        $(this).next('span').html(name);
-    });
-
-    $('.addAnotherForm').click(function (event) {
-        var container = $(this).prev('div.listContainer');
-        var formDiv = $(container).children('div.questionListOfItem').first();
-
-        var cloneForm = $(formDiv.clone());
-        cloneForm.find("input[type='text'],input[type='number']").val('');
-        cloneForm.appendTo(container);
-        cloneForm.children('.removeForm').click(function (event) {
-            var container = $(this).parent('div.questionListOfItem').parent('div.listContainer');
-            if (container.children('div.questionListOfItem').length > 1) {
-                $(this).parent('div.questionListOfItem').remove();
-            } else {
-                $(this).parent('div.questionListOfItem').find("input[type='text'],input[type='number']").val('');
-            }
-        });
-    });
-
-    $('.removeForm').click(function (event) {
-        var container = $(this).parent('div.questionListOfItem').parent('div.listContainer');
-        if (container.children('div.questionListOfItem').length > 1) {
-            $(this).parent('div.questionListOfItem').remove();
-        } else {
-            $(this).parent('div.questionListOfItem').find("input[type='text'],input[type='number']").val('');
-        }
-    });
-
-    $('div.qset').click(function (event) {
-        var url = $(this).data('backurl');
-        window.location = url;
-    });
-
-    $('div.inlineCheck').filter(':even').css('background', '#E5E4E8');
-
-    $('.appList a.delete').click(function (event) {
-        var item = $(this).attr('title');
-        var ok = confirm("Are you sure you want to " + item + "?");
-        if (!ok) {
-            event.preventDefault();
-        }
-        return ok;
-    });
+    var container = $('div.goodFormContainer');
+    //datepicker doesn't like id's that are the same...which is generally bad
+    goodform.fixNerdergFormTagsDatepickers();
+    goodform.addBehaviour(container);
 
 });
