ec8fa98b8bbd1fef6c2d3f8b60d9cae6d36f84b9
pmcneil <peter@nerderg.com>
2014-10-12 23:40:06 +1100

    * include jquery.mousewheel.min.js in the goodform resources module

--- grails-app/conf/GoodFormResources.groovy
+++ grails-app/conf/GoodFormResources.groovy
@@ -3,6 +3,7 @@ modules = {
         dependsOn 'jquery, jquery-ui'
         resource url:[plugin: 'goodForm', dir:'css', file:'goodform.css'], disposition: 'head'
         resource url:[plugin: 'goodForm', dir:'js', file:'goodform.js'], disposition: 'head'
+        resource url:[plugin: 'goodForm', dir:'js', file:"jquery.mousewheel.min.js"], disposition: 'head'
         resource url:[plugin: 'goodForm', dir:'js', file:"jquery.timeentry.min.js"], disposition: 'head'
     }
 }
